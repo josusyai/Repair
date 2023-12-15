73c11eea127a5faa3c4cc8836d967831393e1354
Josus <107114952+josusyai@users.noreply.github.com>
2023-12-15 10:20:48 -0400

    Add files via upload

---
+++ src/josus/Repair.php
@@ -0,0 +1,48 @@
+<?php
+/*
+* :C
+*/
+namespace josus;
+
+use josus\commands\RepairCommand;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\permission\Permission;
+use pocketmine\permission\PermissionManager;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
+
+class Repair extends PluginBase
+{
+    use SingletonTrait;
+
+    protected function onLoad(): void
+    {
+        self::setInstance($this);
+    }
+
+    protected function onEnable(): void
+    {
+        $permissions = [
+            "fix-all.command.use",
+            "fix-hand.command.use",
+            "fix-all-others.command.use",
+        ];
+        $manager = PermissionManager::getInstance();
+        foreach ($permissions as $permission) {
+            $manager->addPermission(new Permission($permission));
+            $manager->getPermission(DefaultPermissions::ROOT_OPERATOR)->addChild($permission, true);
+        }
+        $this->saveResource("messages.yml");
+        $this->getServer()->getCommandMap()->register("repair", new RepairCommand());
+    }
+
+    protected function onDisable(): void{
+        $file = new Config($this->getServer()->getDataPath()."messages.yml",Config::YAML);
+        $file->save();
+    }
+
+    public static function getConfiguration($configuration): Config{
+        return new Config(self::getInstance()->getDataFolder().$configuration, Config::YAML);
+    }
+}
